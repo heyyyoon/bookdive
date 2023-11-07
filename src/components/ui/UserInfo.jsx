65083ec2e14cfa5d66197f6bbea1a3526751387c
heyyyoon <hongji13@naver.com>
2023-11-07 17:23:11 +0900

    modify  mypageo

--- src/components/ui/UserInfo.jsx
+++ src/components/ui/UserInfo.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { ImProfile } from "react-icons/im";
+import { SiGooglehome } from "react-icons/si";
 
-export default function UserInfo({nickname}) {
+export default function UserInfo({ nickname }) {
   return (
-    <div className="flex flex-row items-center">
-      <ImProfile className="text-3xl text-darkgrey"/>
-      <p className="text-2xl font-semibold ml-1 text-medigrey">{nickname}</p>
+    <div className="flex flex-col items-start">
+      <div className="flex flex-row items-end">
+        <SiGooglehome className="text-4xl text-[#574240]" />
+        <p className="text-2xl font-semibold ml-2 text-medigrey">MYPAGE</p>
+      </div>
+      <div className="flex flex-row mt-5">
+      <p className="text-xl font-semibold text-medigrey">안녕하세요,</p>
+        <p className="text-xl font-semibold text-medigrey ml-1">{nickname}</p>
+        <p className="text-xl font-semibold ml-1 text-medigrey">님</p>
+      </div>
     </div>
   );
 }
