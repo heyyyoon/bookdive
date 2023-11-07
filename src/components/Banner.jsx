b2754077b57b8f7a3958eaa140aa5a0cf308cd08
heyyyoon <hongji13@naver.com>
2023-11-07 13:37:59 +0900

    modify banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function Banner() {
   return (
-    <section className='h-60 bg-zinc-400 relative'>
-      <div className='w-full h-full bg-cover bg-banner opacity-50' />
-      <div className='absolute w-full top-20 text-center text-gray-50 drop-shadow-2xl'>
-        <h2 className='text-6xl'>Book Dive</h2>
-        <p className='text-2xl mt-2'>생각을 정리하고 소통하는 공간</p>
+    <section className='h-64 bg-[#575552] relative'>
+      <div className='w-full h-full bg-cover bg-banner opacity-30' />
+      <div className='absolute w-full py-3 top-14 text-center text-gray-50 drop-shadow-2xl'>
+      
+        <h2 className='text-6xl text-[#FFCD4B] font-bold'>Book Dive</h2>
+        <p className='text-2xl text-white font-bold mt-3'>생각을 기록하고 소통하는 공간</p>
+        
       </div>
     </section>
   );
--- src/components/card/ReviewItem.jsx
+++ src/components/card/ReviewItem.jsx
@@ -4,7 +4,7 @@ export default function ReviewItem({ review, styleT, onOpen }) {
   return (
     <li className={styleT} onClick={() => onOpen && onOpen(review)}>
       <div className="h-[35%] flex justify-left items-start">
-        <p className="text-title text-darkgrey font-bold line-clamp-2 text-center ">
+        <p className="text-title text-darkgrey font-bold line-clamp-2 text-left">
           {review.reviewTitle}
         </p>
       </div>
