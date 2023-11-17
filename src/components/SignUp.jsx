2e76d3e04a561fac1e364f3b0ecf569b9272eb60
heyyyoon <hongji13@naver.com>
2023-11-18 01:19:52 +0900

    modi

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -16,17 +16,28 @@ export default function SignUp({ signResult }) {
   };
   const handleSignUp = async (e) => {
     e.preventDefault();
-    try {
-      setLoading(true);
-      await signUp(loginInfo);
-      signResult("회원가입이 되었습니다.");
-    } catch (e) {
-      setWarning(e.message);
-      setTimeout(() => setWarning(null), 1000);
-      setLoading(false);
-    } finally {
-      setLoading(false);
-    }
+    
+    if(loginInfo.password.length <= 5) {
+      setWarning('비밀번호를 6자 이상 입력하세요');
+    } else if (loginInfo.nickname.length <=3 ) {
+      setWarning('닉네임을 4자 이상 입력하세요'); 
+    } else if (loginInfo.nickname.length > 10) {
+      setWarning('닉네임을 10자 이하로 입력하세요');
+    } else {
+      try {
+        setLoading(true);
+        await signUp(loginInfo);
+        signResult("회원가입이 되었습니다.");
+      } catch (e) {
+        setWarning(e.message);
+        setTimeout(() => setWarning(null), 1000);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    }  
+
+    setTimeout(() => setWarning(null), 1000);
   };
   return (
     <div className="relative">
